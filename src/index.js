39ab2ffb38ece5bcf55c5523a4c96465748dd0fc
Dariusz Okoński <darek200180@gmail.com>
2024-09-30 19:14:36 +0200

    add messages

--- src/index.js
+++ src/index.js
@@ -7,8 +7,11 @@ import StarRating from './StarRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={7} />
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+    />
+    <StarRating size={24} color='red' maxRating={10} className='test' />
     <StarRating />
   </React.StrictMode>
 );
